7b65b269f7a07bffe0268e808cfb65560cf9d52c
piradite <veis2k15veis@gmail.com>
2024-08-16 16:13:25 +0200

    feat: save screenshots directly to clipboard, thumbnail generation, code optimization
    - Directly save screenshots to clipboard instead of saving it to a png file first
    - Generate and save a small thumbnail image of the screenshot for notifications
    - Optimize screenshot process

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <X11/cursorfont.h>
 #include <png.h>
 
-#define DEFAULT_PATH "/tmp/screenshot.png"
+#define THUMBNAIL_PATH "/tmp/thumb.png"
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
 void handle_error(const char *msg, int code) {
@@ -20,28 +20,20 @@ XImage *capture_screen(Display *d, Window w, int x, int y, int width, int height
     return XGetImage(d, w, x, y, width, height, AllPlanes, ZPixmap);
 }
 
-void save_image_to_png(XImage *i) {
-    FILE *fp = fopen(DEFAULT_PATH, "wb");
-    if (!fp) {
-        handle_error("Failed to open file for writing", 1);
-    }
-
+void save_image_to_png(XImage *i, FILE *fp) {
     png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     if (!png) {
-        fclose(fp);
         handle_error("Failed to create PNG structure", 1);
     }
 
     png_infop info = png_create_info_struct(png);
     if (!info) {
         png_destroy_write_struct(&png, NULL);
-        fclose(fp);
         handle_error("Failed to create PNG info structure", 1);
     }
 
     if (setjmp(png_jmpbuf(png))) {
         png_destroy_write_struct(&png, &info);
-        fclose(fp);
         handle_error("Failed during PNG creation", 1);
     }
 
@@ -55,7 +47,6 @@ void save_image_to_png(XImage *i) {
     png_bytep row = (png_bytep)malloc(i->width * 3 * sizeof(png_byte));
     if (!row) {
         png_destroy_write_struct(&png, &info);
-        fclose(fp);
         handle_error("Failed to allocate memory for row", 1);
     }
 
@@ -72,31 +63,93 @@ void save_image_to_png(XImage *i) {
 
     free(row);
     png_write_end(png, NULL);
-    fclose(fp);
     png_destroy_write_struct(&png, &info);
 }
 
-void notify_user(const char *message) {
-    // Use a more efficient method if possible
-    char cmd[256];
-    snprintf(cmd, sizeof(cmd), "notify-send -u low -t 1500 -i %s 'Screenshot' '%s'", DEFAULT_PATH, message);
-    system(cmd);
+void save_thumbnail_image(XImage *i) {
+    FILE *fp = fopen(THUMBNAIL_PATH, "wb");
+    if (!fp) {
+        handle_error("Failed to open file for writing thumbnail", 1);
+    }
+
+    int thumb_width = i->width / 16;
+    int thumb_height = i->height / 16;
+
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) {
+        fclose(fp);
+        handle_error("Failed to create PNG structure for thumbnail", 1);
+    }
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) {
+        png_destroy_write_struct(&png, NULL);
+        fclose(fp);
+        handle_error("Failed to create PNG info structure for thumbnail", 1);
+    }
+
+    if (setjmp(png_jmpbuf(png))) {
+        png_destroy_write_struct(&png, &info);
+        fclose(fp);
+        handle_error("Failed during PNG creation for thumbnail", 1);
+    }
+
+    png_init_io(png, fp);
+
+    png_set_compression_level(png, 0);
+
+    png_set_IHDR(png, info, thumb_width, thumb_height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_BASE);
+    png_write_info(png, info);
+
+    png_bytep row = (png_bytep)malloc(thumb_width * 3 * sizeof(png_byte));
+    if (!row) {
+        png_destroy_write_struct(&png, &info);
+        fclose(fp);
+        handle_error("Failed to allocate memory for thumbnail row", 1);
+    }
+
+    for (int y = 0; y < thumb_height; y++) {
+        png_bytep ptr = row;
+        for (int x = 0; x < thumb_width; x++) {
+            int orig_x = x * 16;
+            int orig_y = y * 16;
+            unsigned long pixel = XGetPixel(i, orig_x, orig_y);
+            *ptr++ = (pixel >> 16) & 0xFF;
+            *ptr++ = (pixel >> 8) & 0xFF;
+            *ptr++ = pixel & 0xFF;
+        }
+        png_write_row(png, row);
+    }
+
+    free(row);
+    png_write_end(png, NULL);
+    png_destroy_write_struct(&png, &info);
+    fclose(fp);
 }
 
-void copy_to_clipboard() {
-    // Check if xclip is available and use it efficiently
+void copy_to_clipboard(XImage *i) {
+    FILE *fp = popen("xclip -selection clipboard -t image/png", "w");
+    if (!fp) {
+        handle_error("Failed to open pipe to xclip", 1);
+    }
+
+    save_image_to_png(i, fp);
+
+    pclose(fp);
+}
+
+void notify_user(const char *message) {
     char cmd[256];
-    snprintf(cmd, sizeof(cmd), "xclip -selection clipboard -t image/png -i %s", DEFAULT_PATH);
+    snprintf(cmd, sizeof(cmd), "notify-send -u low -t 1500 -i %s 'Screenshot' '%s'", THUMBNAIL_PATH, message);
     system(cmd);
 }
 
 void screenshot_whole_screen(Display *d) {
     XImage *i = capture_screen(d, DefaultRootWindow(d), 0, 0, DisplayWidth(d, DefaultScreen(d)), DisplayHeight(d, DefaultScreen(d)));
-    save_image_to_png(i);
+    save_thumbnail_image(i);
+    notify_user("Screenshot taken");
+    copy_to_clipboard(i);
     XDestroyImage(i);
-
-    notify_user("Screenshot saved and copied to clipboard");
-    copy_to_clipboard();
 }
 
 Window get_active_window(Display *d) {
@@ -121,12 +174,10 @@ void screenshot_active_window(Display *d) {
     XWindowAttributes attr;
     XGetWindowAttributes(d, w, &attr);
     XImage *i = capture_screen(d, w, 0, 0, attr.width, attr.height);
-
-    save_image_to_png(i);
+    save_thumbnail_image(i);
+    notify_user("Screenshot taken");
+    copy_to_clipboard(i);
     XDestroyImage(i);
-
-    notify_user("Screenshot saved and copied to clipboard");
-    copy_to_clipboard();
 }
 
 void screenshot_selected_area(Display *d) {
@@ -159,11 +210,10 @@ void screenshot_selected_area(Display *d) {
     int start_y = y1 < y2 ? y1 : y2;
 
     XImage *i = capture_screen(d, root, start_x, start_y, width, height);
-    save_image_to_png(i);
+    save_thumbnail_image(i);
+    notify_user("Screenshot taken");
+    copy_to_clipboard(i);
     XDestroyImage(i);
-
-    notify_user("Screenshot saved and copied to clipboard");
-    copy_to_clipboard();
 }
 
 int main(int argc, char *argv[]) {
